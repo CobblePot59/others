5c2165a232f56abee00ebe6c5bcee287669539b7
CobblePot59 <38343920+CobblePot59@users.noreply.github.com>
2021-05-15 17:53:29 +0200

    Add files via upload

---
+++ digispark/digispark-master/Digispark_examples/Digispark_Admin.ino
@@ -0,0 +1,33 @@
+﻿#include <DigiKeyboard.h>
+#define KEY_Tab    43
+
+void setup() {
+// Parfois nécessaire pour initialiser le clavier
+  DigiKeyboard.sendKeyStroke(0);
+
+// On attend deux petites secondes (pour permettre au driver de se charger)
+  DigiKeyboard.delay(2000);
+
+// On envoie les identifiants
+  DigiKeyboard.print("domaine\\Administrateur");
+  DigiKeyboard.sendKeyStroke(KEY_Tab);
+
+  DigiKeyboard.print("Password1");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+}
+
+void loop() {
+  DigiKeyboard.delay(10000);
+}
+
+
+
+
+
+
+
+
+
+
+
+
